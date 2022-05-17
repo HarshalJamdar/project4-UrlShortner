e8b6857e3e8dc82f9539c5c5de04d832496be0b3
PranayRaut-01 <102968236+PranayRaut-01@users.noreply.github.com>
2022-05-18 00:15:15 +0530

    small change in create controller

--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -10,16 +10,16 @@ const createShortUrl= async function(req,res){
         const baseUrl = 'http://localhost:3000'
         
         //==validating request body==//
-        if(Object.keys(req.body)==0) return res.status(400).send({status: false, message: "Invalid request, please provide details"})
+        if(Object.keys(req.body).length==0) return res.status(400).send({status: false, message: "Invalid request, please provide details"})
         
         //==validating long url==//
         let longUrl=req.body.longUrl
         if (!validUrl.isUri(longUrl)) return res.status(400).send({status: false, message: "Invalid long URL"})
 
         //==ckecking and sending shorturl==//
-        if (validUrl.isUri(longUrl)) {
-            let url = await urlModel.findOne({longUrl:longUrl}).select({_id:0,longUrl:1,shortUrl:1,urlCode:1})
-            if(url) return res.status(200).send({status: true, data : url})
+        
+        let url = await urlModel.findOne({longUrl:longUrl}).select({_id:0,longUrl:1,shortUrl:1,urlCode:1})
+        if(url) return res.status(200).send({status: true, data : url})
 
         //==creating shorturl and url document==//
         const urlCode = shortid.generate(longUrl)
@@ -29,7 +29,7 @@ const createShortUrl= async function(req,res){
         //==destructuring and sending only required keys==/
         let data ={longUrl,shortUrl,urlCode}
         return res.status(201).send({status: true, data : data})
-        }
+        
         
     }catch (err) {
      return res.status(500).send({ status: false, error: err.message })
