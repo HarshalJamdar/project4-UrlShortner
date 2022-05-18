33cf1fb07811e425871a152e287dcdfd86dfc771
Harshal Jamdar <jamdarharshal@gmail.com>
2022-05-18 18:37:00 +0530

    formatting

--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -36,18 +36,17 @@ const createShortUrl= async function(req,res){
         if(Object.keys(req.body).length==0) return res.status(400).send({status: false, message: "Invalid request, please provide details"})
 
         //==validating long url==//
-        let longUrl=req.body.longUrl
+        let longUrl=req.body.longUrl.trim()
         if (!validUrl.isUri(longUrl)) return res.status(400).send({status: false, message: "Invalid long URL"})
         
         //==checking cache==//
-        let fetchLongUrl = await GET_ASYNC(`${req.body.longUrl}`)
+        let fetchLongUrl = await GET_ASYNC(`${longUrl}`)
         const newData = JSON.parse(fetchLongUrl)
         if(fetchLongUrl) return res.status(200).send({status: true, data : newData})
 
         //==ckecking and sending shorturl==//
         let url = await urlModel.findOne({longUrl:longUrl}).select({_id:0,longUrl:1,shortUrl:1,urlCode:1})
         await SET_ASYNC(`${req.body.longUrl}`, JSON.stringify(url))
-
         if(url) return res.status(200).send({status: true, data : url})
 
         //==creating shorturl and url document==//
@@ -60,9 +59,7 @@ const createShortUrl= async function(req,res){
 
         //==setting  data in cache and sending response==//
         await SET_ASYNC(`${req.body.longUrl}`, JSON.stringify(data))
-        return res.status(201).send({status: true, data : data})
-        
-        
+        return res.status(201).send({status: true, data : data}) 
     }catch (err) {
      return res.status(500).send({ status: false, error: err.message })
     }
@@ -77,19 +74,16 @@ const getShortUrl = async function (req, res) {
       //==checking for url code in cache==//
       const cachedUrlData = await GET_ASYNC(`${req.params.urlCode}`)
       const parsingData = JSON.parse(cachedUrlData);
-      if(cachedUrlData) {
-          return res.status(307).redirect(parsingData) 
-        }
-
+      if(cachedUrlData) return res.status(307).redirect(parsingData) 
+        
       //==checking for url code in url collection==//
       const urlData = await urlModel.findOne({ urlCode: req.params.urlCode.trim() })  
       if (!urlData)   // doc not found in url collection
           return res.status(404).send({status: false, message: "No URL Found "});
 
-      //==url code found and now setting in cache redirecting to original url==// 
+      //==url code found and now setting in cache and redirecting to original url==// 
       await SET_ASYNC(`${req.params.urlCode}`, JSON.stringify(urlData.longUrl));
       return res.status(307).redirect(urlData.longUrl)    
-
   }
   catch (error) {
       res.status(500).send({ status: false, error: error.message });
